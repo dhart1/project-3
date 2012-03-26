85ded8f75c1620a36f7ea487414b3fc36f740a87
David Hart <dhart1@unca.edu>
2012-03-25 20:40:45 -0400

    got to express homepage

---
+++ app/routes/index.js
@@ -0,0 +1,13 @@
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+  res.render('index', { title: 'Express' })
+};
+
+exports.user = function(req, res) {
+	res.render('index', { title: 'Express'})
+};
+
